38096fef85037092e5e1bd423e3c1e2948f65bd8
vinay <salimattevinay@onjoyride.com>
2022-02-05 23:18:00 +0530

    in middle.#2

--- contracts/StackUpFactory.sol
+++ contracts/StackUpFactory.sol
@@ -15,39 +15,22 @@ contract StackUpFactory is Owner {
         address loanPool,
         uint256 collateralAmount,
         uint256 auctionInterval,
-        uint256 auctionDuration,
         uint8 maxParticipants,
         address tokenAddress,
         address creator,
         uint256 createdAt
     );
 
-    struct ChitSkeleton {
-        uint256 id;
-        uint256 collateralAmount; //max amount chit
-        uint256 auctionInterval; // no of hours
-        uint256 auctionDuration; //no of months
-        uint8 maxParticipants;
-        address tokenAddress;
-        address creator;
-        uint256 createdAt;
-        address [] bidders;
-    }
-
-    mapping(string => ChitSkeleton) public chits;
-
     function addLoanPool(
-        uint256 maximumBidAmount,
-        uint256 auctionInterval,
-        uint256 auctionDuration,
-        uint8 maxParticipants,
+        uint256 bidAmount, // 1,00,000 every month
+        uint256 auctionInterval, // every month
+        uint8 maxParticipants, //10 people
         address token
-    ) private {
+    ) public {
 
         LoanPool newLoanPool = new LoanPool(
-            maximumBidAmount,
+            bidAmount,
             auctionInterval,
-            auctionDuration,
             maxParticipants,
             token
         );
@@ -59,59 +42,13 @@ contract StackUpFactory is Owner {
         emit NewLoanPool(
             totalPools,
             loanPool,
-            maximumBidAmount * maxParticipants,
+            bidAmount,
             auctionInterval,
-            auctionDuration,
             maxParticipants,
             token,
             msg.sender,
             block.timestamp
         );
-    }
-
-
-    function createLoanPool(
-        uint256 maximumBidAmount,
-        uint256 auctionInterval,
-        uint256 auctionDuration,
-        uint8 maxParticipants,
-        address token
-    ) public isModerator {
-
-        ChitSkeleton chit = ChitSkeleton(
-            maximumBidAmount,
-            auctionInterval,
-            auctionDuration,
-            maxParticipants,
-            token,
-            msg.sender,
-            block.timestamp,
-            new address[maxParticipants]
-        );
-        chits[chit.maximumBidAmount + "" + chit.auctionInterval + "" + chit.auctionDuration + "" + chit.maxParticipants] = chit;
-    }
-
-    function addInterest(ChitSkeleton chit) payable {
-        string chitKey = chit.maximumBidAmount + "" + chit.auctionInterval + "" + chit.auctionDuration + "" + chit.maxParticipants;
-        address[] bidders = chits[chitKey].bidders;
-
-        bool doesListContainElement = false;
-        for (uint i = 0; i < bidders.length; i++) {
-            if (msg.sender == bidders[i]) {
-                doesListContainElement = true;
-            }
-        }
-        require(!doesListContainElement, "already bid submitted bro");
-
-        uint minAmount = chit.collateralAmount / chit.maxParticipants / 20;
-        // taking 5%
-        require(msg.value >= minAmount);
-        uint moneyToReturn = msg.value - minAmount;
-
-        if (moneyToReturn > 0)
-            msg.sender.transfer(moneyToReturn);
-
-        chits[chitKey].bidders[bidders.length] = msg.sender;
     }
 
 }
